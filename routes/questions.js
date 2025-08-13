9e83cb9d16a87ed43ab569262a69fc4060559ecd
Luana <luanafialho140@gmail.com>
2025-08-13 12:30:58 -0300

    Limpando main

--- routes/questions.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const questionsController = require("../controllers/questionsControllers");
-
-router.post("/perguntas", questionsController.buscarPergunta);
-
-module.exports = router;
--- routes/results.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const resultsController = require("../controllers/resultsControllers");
-
-router.post("/pontuacao", resultsController.salvarPontuacao);
-router.post("/ranking", resultsController.mostrarRanking);
-
-module.exports = router;
